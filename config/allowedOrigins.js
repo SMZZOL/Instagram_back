15ab61513ef070c5ff5a8851d9405e76e92aee17
SMZZOL <superdavidya@naver.com>
2023-10-29 17:49:57 +0900

    for deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://instagram-front.onrender.com"
+];
 
 module.exports = allowedOrigins;
