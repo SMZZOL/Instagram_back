73697619be78321b222d141406541d5cae0766d4
SMZZOL <superdavidya@naver.com>
2023-11-16 19:31:33 +0900

    EC2(DOCKER) IP CORS ADDED

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://instagram-front.onrender.com"
+    "https://instagram-front.onrender.com",
+    "http://13.209.69.22:3000/"
 ];
 
 module.exports = allowedOrigins;
